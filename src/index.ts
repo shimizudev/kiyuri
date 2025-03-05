fa005e86257369617ed793b45b3fd4e17b854471
Mohtasim Alam <94981761+shimizudev@users.noreply.github.com>
2025-03-05 16:35:05 +0600

    Project setup

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { CommandClient} from 'eris';
+import { Pool } from 'pg';
+import { loadCommands } from './handlers/commandHandler';
+import { loadEvents } from './handlers/eventHandler';
+import { createLogsTable } from './database/logs';
+import { createUserTable } from './database/userModel';
+import { createGuildTable } from './database/guilds';
+
+export const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+// Initialize Discord bot
+export const bot = new CommandClient(process.env.DISCORD_TOKEN!, { intents: ['guilds', 'guildMessages', 'guildMembers', "messageContent", "guildEmojis", "guildMessageReactions"] }, {
+    description: "Aura Bot",
+    owner: "Sohom829",
+    prefix: ["kiyuri", "k!", "k", "@mention", "aura"]
+});
+
+async function start() {
+    await loadCommands(bot);
+    await loadEvents(bot);
+    await bot.connect();
+    await createLogsTable();
+    await createUserTable();
+    await createGuildTable();
+}
+
+start().catch(console.error);
